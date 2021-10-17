859b16bbd46f3c47ee8bcb35fb142c226c5a45f0
Kutalmış Kağan <79145125+Arakhis92@users.noreply.github.com>
2021-10-17 22:56:06 +0300

    Add files via upload

---
+++ Coronavirus.py
@@ -0,0 +1,23 @@
+from turtle import *
+speed(25)
+color('green')
+bgcolor('white')
+cizme=225
+while cizme > 0:
+    left(cizme)
+    forward(cizme * 3)
+    cizme = cizme - 1
+
+speed(15)
+color('black')
+bgcolor('white')
+cizme1=200
+while cizme1 > 0:
+    left(90)
+    forward(cizme1 * 3)
+    cizme1 = cizme1 - 1
+    write('STAY HOME, WEAR MASK, SAVE LIVES')
+
+
+
+
